bb3503c6428fe47b57fa3e0ae0c28e9a1b2879c2
isaacNuflo <isaac.nuflo21@gmail.com>
2019-01-24 16:00:00 -0500

    yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,14 +1,25 @@
 //const fs = require('express');
 //const fs = require('./fs');
-const { crearArchivo } = require('./multiplicar/multiplicar')
+const { argv } = require('./config/yargs');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
-// console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1]
+let comando = argv._[0];
 
-console.log(base);
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+            .then(lista => console.log(lista))
+            .catch(error => console.log(error));
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(error => console.log(error));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
 
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(error => console.log(error));
+// console.log(argv);
+
+// console.log(base);
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let crearArchivo = (base) => {
+let listarTabla = (base, limite) => {
 
     return new Promise((resolve, reject) => {
 
@@ -8,16 +8,59 @@ let crearArchivo = (base) => {
             reject(`El valor introducido ${base} no es un numero`);
             return;
         }
+        if (!Number(limite)) {
+            reject(`El valor introducido ${limite} no es un numero`);
+            return;
+        }
+        if (base <= 0) {
+            reject(`El valor introducido ${base} no puede ser negativo o cero`);
+            return;
+        }
+        if (limite <= 0) {
+            reject(`El valor introducido ${limite} no puede ser negativo o cero`);
+            return;
+        }
 
         let data = ''
 
-        for (let index = 1; index <= 10; index++) {
+        for (let index = 1; index <= limite; index++) {
+            data += `${base} * ${index} = ${base*index} \n`;
+        }
+        resolve(data);
+    })
+
+}
+
+let crearArchivo = (base, limite = 10) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un numero`);
+            return;
+        }
+        if (!Number(limite)) {
+            reject(`El valor introducido ${limite} no es un numero`);
+            return;
+        }
+        if (base <= 0) {
+            reject(`El valor introducido ${base} no puede ser negativo o cero`);
+            return;
+        }
+        if (limite <= 0) {
+            reject(`El valor introducido ${limite} no puede ser negativo o cero`);
+            return;
+        }
+
+        let data = ''
+
+        for (let index = 1; index <= limite; index++) {
             data += `${base} * ${index} = ${base*index} \n`;
         }
 
-        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+        fs.writeFile(`tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
             if (err) reject(err)
-            else resolve(`tablas/tabla-${base}.txt`);
+            else resolve(`tablas/tabla-${base}-al-${limite}.txt`);
         });
 
     });
@@ -25,5 +68,6 @@ let crearArchivo = (base) => {
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
