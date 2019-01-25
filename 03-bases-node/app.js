389612af4615915df16b1b081de571ba88d2cc87
isaacNuflo <isaac.nuflo21@gmail.com>
2019-01-25 11:41:34 -0500

    tablas excluidas

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,7 @@
 //const fs = require('express');
 //const fs = require('./fs');
 const { argv } = require('./config/yargs');
+const colors = require('colors');
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
@@ -8,12 +9,12 @@ let comando = argv._[0];
 switch (comando) {
     case 'listar':
         listarTabla(argv.base, argv.limite)
-            .then(lista => console.log(lista))
+            .then(lista => console.log(lista.green))
             .catch(error => console.log(error));
         break;
     case 'crear':
         crearArchivo(argv.base, argv.limite)
-            .then(archivo => console.log(`Archivo creado: ${archivo}`))
+            .then(archivo => console.log(`Archivo creado: ${archivo.green}`))
             .catch(error => console.log(error));
         break;
     default:
